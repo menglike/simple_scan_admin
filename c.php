f30a915d475cb3c00973ed0b977ee96f9d5435ab
liang <liang@gutou.com>
2017-11-20 15:46:41 +0800

    初始化

---
+++ c.php
@@ -0,0 +1,36 @@
+<?php
+/**
+        *
+        *简易后台地址扫描器
+        * by 渔夫
+        * date 2017-11-20
+        * v0.1
+*/
+        #扫描文件名，可以自行扩展
+        $file_array = array(
+                'a.txt',
+                'b.txt',
+                'robots.txt',
+        );
+        #扫描目录 ，可以自行扩展
+        $dir_array = array(
+                '/',
+                '/a/',
+                '/m/',
+                '/c/',
+        );
+        #请求地址
+        $url = 'curl -I -m 10 -o /dev/null -s -w %{http_code} http://www.zbj.com';
+        #循环扫描，结果可以输出整理成文本
+        foreach($dir_array as $k=>$v){
+            foreach($file_array as $key=>$val){
+                    $code = $res = null;
+                    echo $curl = $url.$v.$val;
+                    exec($curl,$code,$res);
+                    if($code[0]==200){
+                           echo  '-ok!'.PHP_EOL; 
+                    }else{
+                           echo   $code[0].'-no!'.PHP_EOL;
+                    }
+            }
+        }
